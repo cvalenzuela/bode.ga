6a03b03dc055cf53ad9cfab49edb3726222170b5
Cristobal Valenzuela <cvalenzuela@nyu.edu>
2017-10-12 13:53:00 -0400

    add bad coke

--- viewModels/bundle/index.js
+++ viewModels/bundle/index.js
@@ -44248,7 +44248,7 @@ const THREE = require('three');
 const utils = require('./utils');
 
 // add all objects here
-let objects = ['dietcoke', 'brownBag', 'croissant02', 'oatmeal', 'hamTomato','empanada', 'croissantChesse', 'creamCheese', 'coffee02','coffee01','bagelButter', 'banana', 'bag', 'chips', 'croissant','hamTurkey'];
+let objects = ['coke', 'brownBag', 'croissant02', 'oatmeal', 'hamTomato','empanada', 'croissantChesse', 'creamCheese', 'coffee02','coffee01','bagelButter', 'banana', 'bag', 'chips', 'croissant','hamTurkey'];
 let loadedModels = {};
 let container, camera, scene, renderer, currentObject, manager;
 let windowHalfX = window.innerWidth / 2;
